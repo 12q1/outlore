0a8d09b841de38a4acc5b45b06d3e8d55a946b6f
12q1 <thomasham89@gmail.com>
2020-02-21 18:18:05 +0100

    added patch method to /user endpoint

--- server/src/routes/router_user.js
+++ server/src/routes/router_user.js
@@ -4,13 +4,16 @@ const { toJWT, toData } = require('../utils/jwt')
 const auth = require('../middleware/middleware_auth')
 const User = require('../models/model_user')
 const UserSources = require('../models/model_user_sources')
-const db = require('../services/pgdb')
+const { passwordValidation, emailValidation } = require('../utils/validation')
+const bcrypt = require('bcrypt')
 
 // Applying authentication middleware to all routes within this router
 router.use(auth)
 
 // ---Routes related to user---
-router.get('/user/:userId', (req, res) => {
+
+//retrieves user data (eg. email/id/update & creation time but NOT password info)
+router.get('/user/:userId', (req, res, next) => {
     const tokenId = toData(req.headers.authorization.split(' ')[1]).userId //decoding user id stored in JWT token
     const userId = parseInt(req.params.userId)
     if (userId === tokenId) {
@@ -21,7 +24,7 @@ router.get('/user/:userId', (req, res) => {
                 //TODO need to check if other values from the user object are leaking through
                 res.send({ user })
             })
-            .catch(err => console.log(err))
+            .catch(next) //handle error if db fails
     } else {
         res.status(403).send({
             message: "You are not authorized to view this information"
@@ -29,6 +32,7 @@ router.get('/user/:userId', (req, res) => {
     }
 })
 
+// permanently deletes user from db
 router.delete('/user/:userId/delete', (req, res, next) => {
     const tokenId = toData(req.headers.authorization.split(' ')[1]).userId //decoding user id stored in JWT token
     const userId = parseInt(req.params.userId)
@@ -41,7 +45,33 @@ router.delete('/user/:userId/delete', (req, res, next) => {
                 })
             )
             //TODO recursively destroy other table rows connected to userid
-            .catch(err => console.log(err))
+            .catch(next) //handle error if db fails
+    } else {
+        res.status(403).send({
+            message: "You are not authorized to perform this action"
+        })
+    }
+})
+
+// modifies user info 
+router.patch('/user/:userId/patch', (req, res, next) => {
+    const tokenId = toData(req.headers.authorization.split(' ')[1]).userId //decoding user id stored in JWT token
+    const userId = parseInt(req.params.userId)
+    //Validation checks
+    if (req.body.password && !passwordValidation(req.body.password)) return res.status(400).send({ message: 'password is not strong enough' })
+    if (req.body.password) req.body.password = bcrypt.hashSync(req.body.password, 10)
+    //if patch request body contains password then hash it
+    if (req.body.email && !emailValidation(req.body.email)) return res.status(400).send({ message: 'email address is not valid' })
+    if (userId === tokenId) {
+        User
+            .findByPk(userId)
+            .then(user => user.update(req.body))
+            .then(user => res.send({
+                email: user.dataValues.email,
+                created: user.dataValues.createdAt,
+                updatedAt: user.updatedAt
+            })) //omitting password from response
+            .catch(next)
     } else {
         res.status(403).send({
             message: "You are not authorized to perform this action"
@@ -50,20 +80,20 @@ router.delete('/user/:userId/delete', (req, res, next) => {
 })
 
 // ---Routes related to user sources---
-router.get('/user/:userId/sources', (req, res) => {
+router.get('/user/:userId/sources', (req, res, next) => {
     const tokenId = toData(req.headers.authorization.split(' ')[1]).userId //decoding user id stored in JWT token
     const userId = parseInt(req.params.userId)
     if (userId === tokenId) {
         UserSources
             .findOne({ where: { userId: userId } })
             .then(sources => {
-                delete sources.dataValues.id
+                delete sources.dataValues.id //removing unwanted fields from object before returning
                 delete sources.dataValues.userId
                 delete sources.dataValues.updatedAt
                 delete sources.dataValues.createdAt
                 res.send({ sources })
             })
-            .catch(err => console.log(err))
+            .catch(next) //handle error if db fails
     } else {
         res.status(403).send({
             message: "You are not authorized to view this information"
