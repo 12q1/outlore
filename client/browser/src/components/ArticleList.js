e3323af9f429463133ba276801ec76d691e9f346
12q1 <thomasham89@gmail.com>
2020-03-05 22:48:11 +0100

    removed unused mui components

--- client/browser/src/components/ArticleList.js
+++ client/browser/src/components/ArticleList.js
@@ -1,20 +1,14 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
 //import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 //import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import Link from '@material-ui/core/Link';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import IconButton from '@material-ui/core/IconButton';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 //import tileData from './tileData';
 
@@ -28,12 +22,6 @@ const useStyles = makeStyles({
   },
   media: {
     height: 200,
-  },
-  expand: {
-    transform: 'rotate(0deg)',
-  },
-  expandOpen: {
-    transform: 'rotate(180deg)',
   },
   title: {
     fontSize: 16,
@@ -50,16 +38,9 @@ const useStyles = makeStyles({
 
 const ArticleList = (props) => {
   const classes = useStyles();
-  const [expanded, setExpanded] = React.useState(false);
-
   if (props.articles.length === 0) {
     return null
   } else {
-
-    const handleExpandClick = () => {
-      setExpanded(!expanded);
-    };
-
     const renderArticle = (article) => {
       return (
         <Grid item xs key={article.url}>
@@ -82,8 +63,8 @@ const ArticleList = (props) => {
                 {article.author} ({article.source.name})
               </Typography>
               <Typography className={classes.description} color="textSecondary" paragraph>
-                    {article.description}
-                  </Typography>
+                {article.description}
+              </Typography>
               <CardActions>
               </CardActions>
             </CardContent>
