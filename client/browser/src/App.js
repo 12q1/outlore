1e5fd7a1f71e53134369491493581f56a0a1ed0e
12q1 <thomasham89@gmail.com>
2020-03-07 14:48:10 +0100

    added theme provider to root component

--- client/browser/src/App.js
+++ client/browser/src/App.js
@@ -3,17 +3,24 @@ import store from './store'
 import { Provider } from 'react-redux'
 import { Route } from 'react-router-dom'
 //import Home from './components/Home'
-import { ThemeProvider } from '@material-ui/core/styles';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import ArticleListContainer from './components/ArticleListContainer'
 import ResponsiveDrawer from './components/ResponsiveDrawer'
+import blue from '@material-ui/core/colors/blue';
 
-const theme = {};
+const theme = createMuiTheme({
+  palette: {
+    primary: blue,
+  },
+});
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
+        <ThemeProvider theme={theme}>
           <Route path="/" exact component={ResponsiveDrawer} />
+        </ThemeProvider>
       </Provider>
     );
   }
