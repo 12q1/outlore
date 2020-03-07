31a0044405a038ce996268b2859916f7b50cbf27
12q1 <thomasham89@gmail.com>
2020-03-07 01:14:00 +0100

    added responsive drawer component

--- client/browser/src/App.js
+++ client/browser/src/App.js
@@ -4,13 +4,14 @@ import { Provider } from 'react-redux'
 import { Route } from 'react-router-dom'
 //import Home from './components/Home'
 import ArticleListContainer from './components/ArticleListContainer'
+import ResponsiveDrawer from './components/ResponsiveDrawer'
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
         <div>
-          <Route path="/" exact component={ArticleListContainer} />
+          <Route path="/" exact component={ResponsiveDrawer} />
         </div>
       </Provider>
     );
